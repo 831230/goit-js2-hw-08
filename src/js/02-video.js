7c47a748aa597dce9dc635b2498fb8e417beca88
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-10-01 12:12:40 +0200

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,29 @@
+import Player from '@vimeo/player';
 
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-  
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-const onPlay = function(evt) {
-  console.log(evt);
-  // data is an object containing properties specific to that event
-};
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
-player.on('play', onPlay);
-// player.on('eventName', onPlay);
+player.on("timeupdate", throttle((data) => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
+console.log('Czas: ' + localStorage.getItem('videoplayer-current-time'));
