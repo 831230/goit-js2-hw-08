c69fa3569bd94f8722d40b4ce657faa2fb9d2555
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-10-01 13:54:48 +0200

    task no 3 commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+var throttle = require('lodash.throttle'); 
 
+const feedbackForm=document.querySelector(".feedback-form");
+const localstorageKey="feedback-form-state";
+
+if (localStorage.getItem(localstorageKey)) {
+    const emailInStorage=localStorage.getItem(localstorageKey);
+    const messageInStorage=localStorage.getItem(localstorageKey);
+    console.log(JSON.parse(emailInStorage).email, JSON.parse(messageInStorage).message);
+    feedbackForm.elements.email.value=JSON.parse(emailInStorage).email;
+    feedbackForm.elements.message.value=JSON.parse(messageInStorage).message;
+};
+
+feedbackForm.addEventListener("input",function(evt){
+  evt.preventDefault();
+  const formObject={
+    email:feedbackForm.elements.email.value,
+    message:feedbackForm.elements.message.value
+  };
+  localStorage.setItem(localstorageKey,JSON.stringify(formObject));
+})
+
+feedbackForm.addEventListener("submit",function(evt){
+  evt.preventDefault();
+  const formObject={
+    email:feedbackForm.elements.email.value,
+    message:feedbackForm.elements.message.value
+  };
+  console.log(formObject);
+  feedbackForm.reset();
+  localStorage.clear()
+})
