4cf4ff3467c46a8e9871eef81e394fd6754955d6
831230 <97232146+831230@users.noreply.github.com>
2022-10-01 10:13:17 +0000

    Deploying to gh-pages from @ 831230/goit-js2-hw-08@7c47a748aa597dce9dc635b2498fb8e417beca88 🚀

--- 02-video.e73755eb.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("iframe"),e=new Vimeo.Player(o);e.on("play",(function(){console.log("played the video!")})),e.getVideoTitle().then((function(o){console.log("title:",o)}));e.on("play",(function(o){console.log(o)}))}();
-//# sourceMappingURL=02-video.e73755eb.js.map
